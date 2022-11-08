84a1adf5ed0a5034f0dd0808c1c767c3c68a46df
DenisYakimenko <dyakimenko@mail.ru>
2022-11-09 02:09:26 +0300

    add Cell.java

---
+++ src/com/company/Cell.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Cell {
+
+    Status status;
+
+    public Cell(){
+        status = Status.NONE;
+    }
+
+    void step1(){
+
+    }
+
+    void step2(){
+
+    }
+}
