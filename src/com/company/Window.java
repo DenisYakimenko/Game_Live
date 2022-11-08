7428329bd9c1b914206cc2aca9c3b5652eb471e2
DenisYakimenko <dyakimenko@mail.ru>
2022-11-09 02:03:52 +0300

    add Box.java

--- src/com/company/Window.java
+++ src/com/company/Window.java
@@ -6,20 +6,34 @@ import java.io.ObjectInputFilter;
 public class Window implements Runnable{
 
     JFrame frame;
+    Box[][] boxes;
 
     @Override
     public void run(){
         initFrame();
+        initBoxes();
 
     }
+
+
 
     void initFrame() {
         frame = new JFrame();
         frame.getContentPane().setLayout(null);
-        frame.setSize(Config.SIZE * Config.WIDTH, Config.SIZE * Config.HEIGHT);
+        frame.setSize(Config.SIZE * Config.WIDTH+25, Config.SIZE * Config.HEIGHT+45);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         frame.setTitle("Life Game");
+    }
+
+    void initBoxes() {
+        boxes = new Box[Config.WIDTH][Config.HEIGHT];
+        for (int x = 0; x<Config.WIDTH; x++)
+            for (int y = 0; y< Config.HEIGHT; y++)
+            {
+                boxes [x][y] = new Box(x,y);
+                frame.add(boxes[x][y]);
+            }
     }
 }
