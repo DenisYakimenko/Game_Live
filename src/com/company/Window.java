6400228a47fd794978b282c0d8560e642615112a
DenisYakimenko <dyakimenko@mail.ru>
2022-11-09 00:36:21 +0300

    Initial commit

---
+++ src/com/company/Window.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Window implements Runnable{
+
+    JFrame frame;
+
+    @Override
+    public void run(){
+        initFrame();
+
+    }
+
+    void initFrame() {
+        frame = new JFrame();
+        frame.getContentPane().setLayout(null);
+        frame.setSize(100,200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setTitle("Life Game");
+    }
+}
